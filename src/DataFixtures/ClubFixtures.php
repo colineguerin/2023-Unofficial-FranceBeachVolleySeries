c817dbfc1586dec650d494fc2d8f3c67755dbcb2
colineguerin <guerin.coline@gmail.com>
2023-07-01 16:29:24 +0200

    club and article fixtures

---
+++ src/DataFixtures/ClubFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Club;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class ClubFixtures extends Fixture
+{
+    public const CLUBS = [
+        [
+            'name' => 'Ré Beach Club'
+        ],
+        [
+            'name' => 'Bordeaux Beach Chillers'
+        ],
+        [
+            'name' => 'Montpellier Beach Volley'
+        ],
+        [
+            'name' => 'Sand System Association'
+        ],
+        [
+            'name' => '59ers Beach-Volley'
+        ],
+        [
+            'name' => 'Beach Nantes Rezé'
+        ],
+        [
+            'name' => 'Anglet Beach Bask'
+        ],
+        [
+            'name' => 'Beach Volley Toulousain'
+        ],
+        [
+            'name' => 'Nice Beach Volley'
+        ],
+        [
+            'name' => 'Beach Sport Dijon'
+        ],
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::CLUBS as $key => $value) {
+            $club = new Club();
+            $club->setName($value['name']);
+            $manager->persist($club);
+        }
+        $manager->flush();
+    }
+
+
+}
