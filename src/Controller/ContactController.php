5fb02ea16bf61d21f0dbfc22409979a74a23c4bf
colineguerin <guerin.coline@gmail.com>
2023-07-04 14:59:12 +0200

    contact fixes

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 
 class ContactController extends AbstractController
 {
