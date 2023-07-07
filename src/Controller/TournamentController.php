6ed61925473d7d0313b61f01148b4e2dcaada5b1
colineguerin <guerin.coline@gmail.com>
2023-07-07 17:08:30 +0200

    display registered teams for each tournament

--- src/Controller/TournamentController.php
+++ src/Controller/TournamentController.php
@@ -68,6 +68,7 @@ class TournamentController extends AbstractController
             'tournament' => $tournament,
             'availableSpots' => $availableSpots,
             'completionPercentage' => $completionPercentage,
+            'teams' => $teams,
         ]);
     }
 
