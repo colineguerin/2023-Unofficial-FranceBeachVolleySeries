8ee8bfc672930de3e16afec3c5ce94bfdffa11aa
colineguerin <guerin.coline@gmail.com>
2023-07-12 18:08:22 +0200

    search user

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -2,21 +2,54 @@
 
 namespace App\Controller;
 
-use App\Repository\ResultRepository;
+use App\Form\SearchUserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ResultController extends AbstractController
 {
-    #[Route('/classement', name: 'app_result')]
-    public function index(UserRepository $userRepository): Response
+    #[Route('/classement', name: 'app_result', methods: ['GET', 'POST'])]
+    public function index(UserRepository $userRepository, Request $request): Response
     {
-        $players = $userRepository->findAll();
+        /* Update player total points */
+        $users = $userRepository->findAll();
+        foreach ($users as $user)
+        {
+            $results = $user->getResults();
+            $points = [];
+            foreach ($results as $result)
+            {
+                $points[] = $result->getPoints();
+            }
+
+            $user->setPoint(array_sum($points));
+
+            $userRepository->save($user, true);
+        }
+
+        /* Get first three players */
+        $womanPodium = $userRepository->findWomanPodium();
+        $manPodium = $userRepository->findManPodium();
+
+        /* Search bar */
+        $form = $this->createForm(SearchUserType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $search = $form->getData()['search'];
+            $players = $userRepository->findByName($search);
+        } else {
+            $players = $userRepository->findBy([], ['point' => 'DESC']);
+        }
 
         return $this->render('result/index.html.twig', [
             'players' => $players,
+            'form' => $form,
+            'womanPodium' => $womanPodium,
+            'manPodium' => $manPodium
         ]);
     }
 }
