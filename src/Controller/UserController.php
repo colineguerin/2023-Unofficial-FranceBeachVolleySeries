dc8c644c2e40a169873f5d5f8d7fa32a31344059
colineguerin <guerin.coline@gmail.com>
2023-07-20 17:13:15 +0200

    service to calculate user's ranking

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\TeamRepository;
 use App\Repository\UserRepository;
+use App\Service\RankingService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
@@ -18,16 +19,12 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/joueur')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
-
     #[Route('/{id}/profil', name: 'app_user_show', methods: ['GET', 'POST'])]
-    public function show(User $user, TeamRepository $teamRepository, Security $security, UserRepository $userRepository): Response
+    public function show(
+        User $user,
+        Security $security,
+        RankingService $rankingService,
+    ): Response
     {
         $currentUser = $security->getUser();
 
@@ -41,56 +38,12 @@ class UserController extends AbstractController
 
         $currentDate = new \DateTime();
 
-        // Get user's national rank compared to all users
-        $users = $userRepository->findAll();
-        $points = [];
-        foreach ($users as $user) {
-            $points[] = $user->getPoint();
-        }
-        $userPoint = $currentUser->getPoint();
-
-        rsort($points);
-        $index = array_search($userPoint, $points);
-        if ($index !== false) {
-            $ranking = $index + 1;
-        } else {
-            $ranking = 0;
-        }
-
         return $this->render('user/show.html.twig', [
             'user' => $user,
             'teams' => $teams,
             'now' => $currentDate,
             'results' => $results,
-            'ranking' => $ranking,
+            'ranking' => $rankingService->getRank($user),
         ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, UserRepository $userRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $userRepository->remove($user, true);
-        }
-
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Service/RankingService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class RankingService
+{
+    private UserRepository $userRepository;
+    private Security $security;
+
+    public function __construct(UserRepository $userRepository, Security $security)
+    {
+        $this->userRepository = $userRepository;
+        $this->security = $security;
+    }
+    public function getRank(User $user): int
+    {
+        $currentUser = $this->security->getUser();
+        $users = $this->userRepository->findAll();
+        $points = [];
+        foreach ($users as $user) {
+            $points[] = $user->getPoint();
+        }
+        $userPoint = $currentUser->getPoint();
+
+        rsort($points);
+        $index = array_search($userPoint, $points);
+        if ($index !== false) {
+            $ranking = $index + 1;
+        } else {
+            $ranking = 0;
+        }
+
+        return $ranking;
+    }
+}
